7f132606bd78ae7946c9fa82206bb14b2ad15d9e
mattcrowther0824 <mattcrowther0824@gmail.com>
2026-06-30 23:49:52 -0500

    Deploy site to Github Pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -8,15 +8,13 @@ def extract_title(markdown):
             return line[2:]
     raise Exception("No h1 header found.")
 
-def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from %s to %s using %s", from_path, dest_path, template_path)
+def generate_page(from_path, template_path, dest_path, basepath):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         md = f.read()
-    f.close()
 
     with open(template_path) as g:
         tmp = g.read()
-    g.close()
 
     new_node = markdown_to_html_node(md)
     html_str = new_node.to_html()
@@ -25,6 +23,8 @@ def generate_page(from_path, template_path, dest_path):
 
     tmp = tmp.replace("{{ Title }}", title)
     tmp = tmp.replace("{{ Content }}", html_str)
+    tmp = tmp.replace('href="/', 'href="{basepath}')
+    tmp = tmp.replace('src="/', 'src="{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -33,14 +33,14 @@ def generate_page(from_path, template_path, dest_path):
     new_file.write(tmp)
     new_file.close()
 
-def generate_pages_recursive(content_dir, template_path, public_dir):
+def generate_pages_recursive(content_dir, template_path, public_dir, basepath):
     for item in os.listdir(content_dir):
         source_path = os.path.join(content_dir, item)
         dest_path = os.path.join(public_dir, item)
 
         if os.path.isdir(source_path):
-            generate_pages_recursive(source_path, template_path, dest_path)
+            generate_pages_recursive(source_path, template_path, dest_path, basepath)
 
         elif source_path.endswith(".md"):
             dest_path = dest_path.replace(".md", ".html")
-            generate_page(source_path, template_path, dest_path)
+            generate_page(source_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,18 @@
+import sys
+
 from copystatic import copy_files_recursive, clear_dest_dir
 from gencontent import generate_page, generate_pages_recursive
 from textnode import TextNode, TextType
 
 
 def main():
-  clear_dest_dir("./public")
-  copy_files_recursive("./static", "./public")
+    if len(sys.argv) > 1:
+      basepath = sys.argv[1]
+    else:
+      basepath = "/"
+    clear_dest_dir("./public")
+    copy_files_recursive("./static", "./public")
 
-  generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", "./docs", basepath)
 
 main()
